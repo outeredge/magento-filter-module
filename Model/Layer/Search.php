e242c558e1c7174c32b6cb1d4b11a70396848819
minlare <mike@outeredgeuk.com>
2017-10-18 10:49:28 +0000

    search results fix

---
+++ Model/Layer/Search.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OuterEdge\Filter\Model\Layer;
+
+use Magento\Catalog\Model\Layer\Search as LayerSearch;
+
+class Search extends LayerSearch
+{
+    public function getCollectionProvider()
+    {
+        return $this->collectionProvider;
+    }
+}
